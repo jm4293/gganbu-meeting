610af730a26ff0ee008593f6b977288e06114a74
jm4293 <dlwoals4293@gmail.com>
2026-03-05 00:37:52 +0900

    style: 현재 시간 컴포넌트 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-'use client';
+"use client";
 
-import { useStocks } from '@/hooks/useStocks';
-import StockOverlay from '@/components/StockOverlay';
-import ChatPanel from '@/components/ChatPanel';
-import DateTimeDisplay from '@/components/DateTimeDisplay';
-import { STOCK_CODES } from '@/lib/constants';
+import { useStocks } from "@/hooks/useStocks";
+import StockOverlay from "@/components/StockOverlay";
+import ChatPanel from "@/components/ChatPanel";
+import DateTimeDisplay from "@/components/DateTimeDisplay";
+import { STOCK_CODES } from "@/lib/constants";
 
 export default function Home() {
   const { stocks, isLoading } = useStocks();
@@ -15,7 +15,9 @@ export default function Home() {
       <div className="main-content">
         {/* 로딩 상태 표시 */}
         {isLoading && (
-          <div className="connection-status disconnected">🔄 데이터 로딩 중...</div>
+          <div className="connection-status disconnected">
+            🔄 데이터 로딩 중...
+          </div>
         )}
 
         <div className="main-background">
@@ -28,13 +30,13 @@ export default function Home() {
 
             {/* 주가 오버레이 */}
             <StockOverlay stock={stocks[STOCK_CODES.SAMSUNG]} position="left" />
-            <StockOverlay stock={stocks[STOCK_CODES.HYUNDAI]} position="center" />
+            <StockOverlay
+              stock={stocks[STOCK_CODES.HYUNDAI]}
+              position="center"
+            />
             <StockOverlay stock={stocks[STOCK_CODES.NVIDIA]} position="right" />
           </div>
         </div>
-
-        {/* 날짜/시간 표시 */}
-        <DateTimeDisplay />
       </div>
 
       {/* 채팅 패널 */}
